56ad8712c1e74d5e413d892f6ff9523e2ba2f208
JPedro0 <jpedro200101@gmail.com>
2022-12-05 22:38:17 -0700

    dockerizar?

--- src/Database/Connection.ts
+++ src/Database/Connection.ts
@@ -5,11 +5,11 @@ import { Consumo } from 'src/Entity/Usage';
 
 export const Connection = TypeOrmModule.forRoot({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'DB-PF-TS',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [Cliente, Consumo, Pago],
   synchronize: false //Desactivado por causa a un pequeño bug con mi sql -Pedro
 });
